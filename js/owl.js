007301ea6bf804f8ebfb1e2be410a8c8d2a581bc
websolid <107296019+websolid@users.noreply.github.com>
2022-09-08 14:37:47 +1200

    Update owl.js

--- js/owl.js
+++ js/owl.js
@@ -2,14 +2,13 @@ $(document).ready(function () {
 
 
 
-$(function(){
-     var navMain = $(".navbar-collapse"); // avoid dependency on #id
-     // "a:not([data-toggle])" - to avoid issues caused
-     // when you have dropdown inside navbar
-     navMain.on("click", "a:not([data-toggle])", null, function () {
-         navMain.collapse('hide');
-     });
- });
+  $('#port').click(function(){
+       var navMain = $(".navbar-collapse"); // avoid dependency on #id
+       // "a:not([data-toggle])" - to avoid issues caused
+       // when you have dropdown inside navb
+           navMain.collapse('hide');
+       });
+
 
  $('#submit').click(function(){
   var contacts = [ "Grace", "grace", "Simon"];
